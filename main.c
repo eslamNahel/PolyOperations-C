c93b934b38b970c723b8add1938467263fc08f0e
eslamNahel <eslamoz.nasser@gmail.com>
2021-11-11 06:22:49 +0200

    Add createNewPoly method and fix a bug in the add element method

--- main.c
+++ main.c
@@ -19,10 +19,11 @@ typedef struct Term
 typedef TermPointer Poly;
 
 void addElementToPoly(Poly poly, int coef, int exp);
+Poly createNewPoly();
 
 int main()
 {
-
+    
     return 0;
 }
 
@@ -36,8 +37,41 @@ void addElementToPoly(Poly poly, int coef, int exp)
     {
         newTerm->element.coefficient = coef;
         newTerm->element.exponent = exp;
-        newTerm->next = poly->next;
-        poly->next = newTerm;
-        poly = newTerm;
+        TermPointer last;
+        last = poly;
+        while (last->next)
+        {
+            last = last->next;
+        }
+        newTerm->next = last->next;
+        last->next = newTerm;
     }
 }
+
+
+Poly createNewPoly()
+{
+    Poly newPoly;
+    newPoly = (Poly)malloc(sizeof(struct Term));
+
+    int size, coef, exp;
+    printf("Please enter the size of your poly: ");
+    scanf("%d", &size);
+
+    TermPointer polyHead;
+    if (newPoly)
+    {
+        newPoly->next = NULL;
+        polyHead = newPoly;
+
+        for (int i = 0; i < size; i++)
+        {
+            printf("Enter the coefficent and exponent of element %d: ", i+1);
+            scanf("%d %d", &coef, &exp);
+            addElementToPoly(polyHead, coef, exp);
+        }
+    }
+    return newPoly;
+}
+
+
